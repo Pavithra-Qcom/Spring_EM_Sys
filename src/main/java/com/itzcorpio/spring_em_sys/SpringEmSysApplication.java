9c4749c2975dea8e9bee4b1ffcd720d893712fb3
Pavithra Chamod J <roguerevengerpcj2@gmail.com>
2024-08-08 19:14:07 +0530

    first commit

---
+++ src/main/java/com/itzcorpio/spring_em_sys/SpringEmSysApplication.java
@@ -0,0 +1,13 @@
+package com.itzcorpio.spring_em_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itzcorpio/spring_em_sys/SpringEmSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzcorpio.spring_em_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
